ab98ea9c3a9ce77efd7569cc64d0c4e1cea0f583
Mohammed Alkuwari <Mohdalkuwari7@gmail.com>
2023-12-08 15:20:11 +0000

    added code

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,16 @@
 """Forms of the project."""
 
 # Create your forms here.
+from django import forms
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+
+    # Customizing widget for description and quantity fields
+    widgets = {
+        'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+        'quantity': forms.NumberInput(attrs={'min': 0, 'max': 100}),
+    }
--- things/views.py
+++ things/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .forms import ThingForm
+from .models import Thing
+
 
 def home(request):
-    return render(request, 'home.html', {'form': form})
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect or do something else after successfully processing the form
+            return redirect('home')
+    else:
+        form = ThingForm()
+
+    return render(request, 'things/home.html', {'form': form})
